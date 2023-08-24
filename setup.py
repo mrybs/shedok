32fc384528c51b2bebbf36a3fda96e903a4c5fcf
mrybs <mrybs@github.com>
2023-08-24 20:56:43 +0300

    0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='shedok',
-	  version='0.1.0',
+	  version='0.1.1',
 	  description='Pending tasks management',
 	  packages=['shedok'],
 	  author_email='mrybs2@gmail.com',
